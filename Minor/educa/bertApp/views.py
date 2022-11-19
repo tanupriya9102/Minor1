8b5413b45ce62feb7d823ebe7e819e5a5204dc64
tanupriya9102 <pathak.tanupriya@gmail.com>
2022-11-19 19:36:32 +0530

    first commit

---
+++ Minor/educa/bertApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return HttpResponse("this is about page")
+def contact(request):
+    return HttpResponse("this is contact page")
