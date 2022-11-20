5bfe62c792e074c59523d6656727e8486a578b30
tanupriya9102 <pathak.tanupriya@gmail.com>
2022-11-20 11:53:22 +0530

    first commit

--- Minor/educa/bertApp/urls.py
+++ Minor/educa/bertApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.index,name='index'),
     path("index",views.index,name='index'),
     path("about",views.about,name='about'),
-    path("contact",views.contact,name='contact')
+    path("contact",views.contact,name='contact'),
+    path("form",views.form,name='form'),
 ]
--- Minor/educa/bertApp/views.py
+++ Minor/educa/bertApp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def form(request):
+    return render(request,'form.html')
 def about(request):
     return render(request,'about.html')
 def contact(request):
